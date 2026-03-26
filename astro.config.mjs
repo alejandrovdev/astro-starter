8171e759c0dd34303356ecc18187ce69b6219a9f
Alejandro Villanueva <ing.alejandrovf@gmail.com>
2026-03-25 19:42:10 -0600

    feat(seo): add sitemap, robots.txt, and meta tags

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,19 @@
 // @ts-check
 import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
+import { loadEnv } from 'vite';
+
+const { SITE_URL } = loadEnv(process.env.NODE_ENV ?? '', process.cwd(), '');
 
 // https://astro.build/config
 export default defineConfig({
   devToolbar: {
     enabled: false,
   },
-  integrations: [react()],
+  integrations: [react(), sitemap()],
+  site: SITE_URL,
   vite: {
     plugins: [tailwindcss()],
   },
